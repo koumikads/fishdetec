b095ddf6d6df42dca030fe1b97cdd4df75ced219
kadija-kru <ntiechekadi@gmail.com>
2023-10-08 04:23:49 -0700

    template

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='fishDetection',
+    version='0.0.0',
+    author='Ntieche Kadija',
+    author_email='ntiechekadija@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
